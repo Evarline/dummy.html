2845da3856dbf88be2f8181dae717674537fccc2
Everline Mipata <everlinemipata3@gmail.com>
2026-02-06 19:11:27 +0300

    center-aligned current status

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const countdownFunction = setInterval(() => {
         document.querySelector(".timer").style.display = "none";
          document.querySelector(".current-status").style.display = "none";
          document.querySelector(".message").style.display = "none";
-    }
+   }
     }, 1000);
          
 
